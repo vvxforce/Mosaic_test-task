12aa181ed1c71a8d33fc90ae4e42074a6384d42b
vvxforce <vvxforce@gmail.com>
2020-03-13 18:06:40 +0200

    Updates

---
+++ precache-manifest.80dc5890a5d9bf96b68baa64d7734faf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54df64df8a7cdab70d264178b13370b4",
+    "url": "/mosaic_test-task/index.html"
+  },
+  {
+    "revision": "800d70b04befa7c128d6",
+    "url": "/mosaic_test-task/static/css/main.4461da66.chunk.css"
+  },
+  {
+    "revision": "992dcfa30da65c4d9f54",
+    "url": "/mosaic_test-task/static/js/2.ecb926a8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/mosaic_test-task/static/js/2.ecb926a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "800d70b04befa7c128d6",
+    "url": "/mosaic_test-task/static/js/main.e3000d7f.chunk.js"
+  },
+  {
+    "revision": "f32c374343d915d0cf7b",
+    "url": "/mosaic_test-task/static/js/runtime-main.c164679a.js"
+  }
+]);
